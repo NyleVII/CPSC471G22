4e9d2efcee8b7ca207fe22a9b29191d161a49ba7
Alex Thompson <alexcthomp@gmail.com>
2016-12-07 18:15:21 -0700

    Delete Review is implemented.

--- app/src/main/java/com/example/ark/myapplication/SingleReviewActivity.java
+++ app/src/main/java/com/example/ark/myapplication/SingleReviewActivity.java
@@ -1,13 +1,17 @@
 package com.example.ark.myapplication;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.TextView;
 
 import java.sql.Connection;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
+
+import static com.example.ark.myapplication.R.styleable.View;
 
 public class SingleReviewActivity extends AppCompatActivity {
 
@@ -45,5 +49,20 @@ public class SingleReviewActivity extends AppCompatActivity {
         }catch (SQLException e){
             e.printStackTrace();
         }
+    }
+
+    public void deleteReview(View view) {
+
+            try {
+                String query = "DELETE FROM " + db + ".dbo.Reviews WHERE ReviewID=" + reviewId;
+                Statement stmt = conn.createStatement();
+                stmt.executeUpdate(query);
+            } catch (SQLException e) {
+                e.printStackTrace();
+            }
+
+            Intent intent = new Intent(this, MainActivity.class);
+            startActivity(intent);
+
     }
 }
